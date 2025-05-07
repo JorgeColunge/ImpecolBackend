08e167282056118c29f2758d72012ffcdf428f3f
Jorge Colunge <cto.axiomarobotics@gmail.com>
2025-05-07 13:12:08 -0500

    xx

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -8071,8 +8071,8 @@ router.post('/get-onlyoffice-config', upload.single('file'), async (req, res) =>
     console.log('📥 Archivo recibido:', file.originalname);
     console.log('🧾 Tipo MIME:', file.mimetype);
     console.log('📍 Ruta local:', file.path);
-    //const publicUrl = `http://host.docker.internal:10000/temp/${file.filename}`
-    const publicUrl = `https://services.impecol.com:10000/temp/${file.filename}`;
+    const publicUrl = `http://host.docker.internal:10000/temp/${file.filename}`
+    //const publicUrl = `https://services.impecol.com:10000/temp/${file.filename}`;
     console.log('🌐 URL accesible desde OnlyOffice:', publicUrl);
 
     const config = {
@@ -8089,7 +8089,7 @@ router.post('/get-onlyoffice-config', upload.single('file'), async (req, res) =>
           id: 'admin',
           name: 'Administrador'
         },
-        callbackUrl: `https://services.impecol.com:10000/temp/${file.filename}`
+        callbackUrl: ''
       }
     };
 
